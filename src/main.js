e2af8622ce37327ba526d27f9f960230d681da70
yexiaojian <yexiaojian@binmade.cn>
2023-08-04 02:36:17 +0800

    feat: 添加歌词等特效

--- src/main.js
+++ src/main.js
@@ -25,6 +25,10 @@ var colors = [
         [1, '#f90'],
         [1, '#ff0'],
     ],
+    [
+        '#a3f3a7',
+        '#a3f3a7'
+    ],
     ['#06f', '#09f', ' #0Cf', '#0ff'],
     ['#fb6d6b', '#c10056', ' #a50053', '#51074b'],
     [
@@ -44,6 +48,7 @@ const defaultEffect = 'circlebar'
 var prettify = false;
 var canvas = $('#canvas');
 var audio = $('#audio');
+
 
 var vudio = new Vudio(audio, canvas, {
     effect: defaultEffect,
@@ -93,23 +98,69 @@ var vudio = new Vudio(audio, canvas, {
     },
 });
 
+// 底部渲染
+var canvas2 = $('#canvas2');
+var audio2 = $('#audio2');
+var vudio2 = new Vudio(audio2, canvas2, {
+    effect: types[2],
+    accuracy: 128,
+    width: 800,
+    height: 800,
+    circlebar: {
+        circleRadius: 200,
+        fadeSide: false,
+        shadowBlur: 4,
+        shadowColor: 'rgba(244,244,244,.5)',
+        coverImg: './demo_src/o_1.png',
+        color: colors[3],
+        verticalAlign: 'bottom'
+
+    },
+    circlewave: {
+        circleRadius: 200,
+        fadeSide: true,
+        shadowBlur: 4,
+        shadowColor: 'rgba(244,244,244,.5)',
+        coverImg: './demo_src/o_2.png',
+        color: colors[0],
+        verticalAlign: 'bottom'
+    },
+    waveform: {
+        maxHeight: 160,
+        spacing: 1,
+        shadowBlur: 6,
+        shadowColor: 'rgba(255,21,10,0.6)',
+        prettify: true,
+        fadeSide: true,
+        color: colors[0],
+        verticalAlign: 'bottom'
+
+    },
+    lighting: {
+        maxHeight: 160,
+        spacing: 1,
+        shadowBlur: 6,
+        shadowColor: 'rgba(255,21,10,0.6)',
+        prettify: true,
+        fadeSide: true,
+        color: colors[4],
+        verticalAlign: 'bottom'
+
+    },
+});
 
 
-function $(selector) {
-    return document.querySelector(selector);
-}
 function changeType(index) {
     if ([1, 3].includes(index)) {
         canvas.style.position = "absolute";
         canvas.style.top = "50%";
         canvas.style.left = "50%";
-        canvas.style.marginLeft = "300px";
+        canvas.style.marginLeft = "100px";
         canvas.style.marginTop = "-300px";
         canvas.style.width = "600px";
         canvas.style.height = "600px";
 
     } else {
-
         canvas.style.top = '45%';
         canvas.style.left = '5%';
         canvas.style.width = '90%';
@@ -157,8 +208,40 @@ function prettifyWaveform() {
     prettify = !prettify;
 }
 
+let isPlay = false
+
+const controller = $('.controller')
+
+const stopMusic = () => {
+    audio.pause();
+    audio2.pause();
+    isPlay = false
+    controller.style.display = 'block'
+}
+
+
+
+var element = document;
+
+// 添加键盘事件监听器
+element.addEventListener("keydown", function (event) {
+    // 获取按下的键的键码
+    var keyCode = event.keyCode || event.which;
+
+    // 判断是否按下的是空格键
+    if (keyCode === 32) {
+        if (!isPlay) {
+            playMusic()
+        } else {
+            stopMusic()
+        }
+
+    }
+});
+
 const main = () => {
     vudio.dance();
+    vudio2.dance();
 }
 
 main()
--- src/tools.js
+++ src/tools.js
@@ -3,4 +3,6 @@ function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// 调用函数生成随机数
+function $(selector) {
+  return document.querySelector(selector);
+}
