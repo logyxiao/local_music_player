3bfa933f09a6bce6951010d1043346034474c444
yexiaojian <yexiaojian@binmade.cn>
2023-08-04 01:17:48 +0800

    feat: 调整位置

--- src/colorsSelect.js
+++ src/colorsSelect.js
@@ -1,41 +1,40 @@
 
 
-const getLinearGradient = (gradientArray)=>{
+const getLinearGradient = (gradientArray) => {
     //  如果gradientArray是数组
     if (Array.isArray(gradientArray[0])) {
-     gradientString = gradientArray.map(([percentage, color]) => `${color} ${percentage * 100}%`).join(', ');
+        gradientString = gradientArray.map(([percentage, color]) => `${color} ${percentage * 100}%`).join(', ');
 
-    }else{
-             gradientString = gradientArray.map(( color) => `${color}`)
+    } else {
+        gradientString = gradientArray.map((color) => `${color}`)
 
     }
-
 
     return `linear-gradient(${gradientString})`;
 }
 
 function renderButtons() {
 
-  var container = document.getElementById("colors-select");
+    var container = document.getElementById("colors-select");
 
-  for (var i = 0; i < colors.length; i++) {
-    var button = document.createElement("button");
-    button.className = "color " + `color-${i+1}`
-    button.style.backgroundImage = getLinearGradient(colors[i])
-    button.addEventListener("click", changeColor.bind(null, i));
-    container.appendChild(button);
-  }
+    for (var i = 0; i < colors.length; i++) {
+        var button = document.createElement("button");
+        button.className = "color " + `color-${i + 1}`
+        button.style.backgroundImage = getLinearGradient(colors[i])
+        button.addEventListener("click", changeColor.bind(null, i));
+        container.appendChild(button);
+    }
 }
 
 function changeColor(index) {
     vudio.setOption({
-        waveform : {
+        waveform: {
             color: colors[index]
         },
-        circlewave : {
+        circlewave: {
             color: colors[index]
         },
-        circlebar : {
+        circlebar: {
             color: colors[index]
         },
         lighting: {
